1acde8702da02d4d6d2e01f5de4490f994cf4bbe
Oramic <167904002+OramicGIT@users.noreply.github.com>
2025-02-25 16:56:08 +0200

    Add files via upload

--- setuphost.bat
+++ setuphost.bat
@@ -1,16 +1,30 @@
 @echo off 
-title OramicCOS Setup
+title OramicCOS Setup App
 
 :: Запрос имени архива
-set /p archive=Enter setup image here (or load backup from e.g Backup001.zip, if 0, make new backup)
+echo -------------------
+echo 1. Load Installation Package (.zip name)
+echo 0. Make Point
+echo -------------------
+set /p archive=X:\
 )
 if %archive%==0 (cd C-\System16\backup
-start MakeBackup.bat
+start PointMaker.bat
 cd..
 cd..
 cd..
 exit
 )
+
+if exist "C-" (
+  echo There is a installed system, Installer need to erase all System folders, are you sure?
+  echo If you not sure, restart this app and make backup of System.
+  timeout /t 10 >nul
+  echo Erasing...
+  del os.bat /s/q
+  rmdir C- /s/q
+)
+pause
 
 :: Запрос целевой папки (по умолчанию текущая папка)
 set /p folder=Enter the folder to extract to (Leave blank to extract here):
@@ -32,8 +46,11 @@ if not exist "%zipPath%" (
     pause
     exit /b
 )
-
-echo Starting DVD copy...
+echo ------------------------------------------------------------
+echo Extracting %archive%... Make sure your PC is plugged in.
+echo.
+echo Please wait until I start the 7-Zip...
+echo ------------------------------------------------------------ 
 
 :: Используем 7-Zip для распаковки архива в целевую папку
 "%zipPath%" x "%archive%" -o"%folder%" -y
@@ -46,9 +63,17 @@ if %errorlevel% neq 0 (
 )
 
 :: Переход в распакованную папку и запуск установки
+echo Melting 7-Zip... I just want to eat. *fire smoke*
 rmdir /s/q backup
+echo Nom-Nom
+timeout /t 1 >nul
+echo Meltdown is ended, PLAY GEOMETRY DASH MEL... Oops, starting OOBE...
+echo Please wait... We are initializing OS kernel... Please wait!
+timeout /t 3 >nul
 cd "%folder%"
-start C-\System16\oobe\oob.bat
+cd C-\System16\oobe
+start oob.bat
 
-echo Installer is unpacked into "%folder%".
-pause
+echo Goodbye!
+timeout /t 1 >nul
+exit
